7ba4970ee106ffdd9a98c61b34a73636bc435cf4
German Montero <germonram@gmail.com>
2025-04-07 17:49:54 -0600

    Updates base URL for broader compatibility
    Removes the '/Portfolio' base URL, allowing the application to be served from the root directory.
    This enhances deployment flexibility and simplifies access.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  base: '/Portfolio',
+  base: '/',
   adapter: node({
     mode: 'standalone'
   })
