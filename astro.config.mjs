251b949b219412d6d5c429d25098f593e97524c1
German montero <germonram@gmail.com>
2025-10-19 19:44:34 -0600

    build: Configure Astro for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import node from '@astrojs/node';
 export default defineConfig({
   output: "server",
   base: '/',
-  adapter: node({ mode: 'standalone' })// vercel( {  }),
+  adapter: vercel( {  }),
 });
